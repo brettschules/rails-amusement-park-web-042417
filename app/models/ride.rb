601847d1db4b654c0c9f66a019368defab1c50f3
Brett Schules <brettschules@gmail.com>
2017-05-25 10:43:40 -0400

    Added migration/models and methods

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,23 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :attraction
+  belongs_to :user
+
+
+
+
+  def take_ride
+    if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
+      return "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+    elsif  self.user.tickets < self.attraction.tickets
+      return "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+    elsif self.user.height < self.attraction.min_height
+      return "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+    else
+       current_ticket_count = self.user.tickets - self.attraction.tickets
+       current_happiness_count = self.user.happiness + self.attraction.happiness_rating
+       current_nausea_count = self.user.nausea + self.attraction.nausea_rating
+
+       self.user.update(tickets: current_ticket_count, happiness: current_happiness_count, nausea: current_nausea_count)
+    end
+  end
 end
---
+++ db/migrate/20170525133730_create_rides.rb
@@ -0,0 +1,10 @@
+class CreateRides < ActiveRecord::Migration
+  def change
+    create_table :rides do |t|
+      t.references :user
+      t.references :attraction
+
+      t.timestamps null: false
+    end
+  end
+end
